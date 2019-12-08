6a90e4edd5c462e39f426fe2ccc0ded65b875dbe
isaackumi <isaackumi@github.io>
2019-12-08 16:09:13 +0000

    add timer

--- view/update.php
+++ view/update.php
@@ -42,6 +42,8 @@ foreach ($user as $value) {
     <title>Update Contact</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="../js/ajaxCalls.js"></script>
+  <script type="text/javascript" src="../js/jquery.js"></script>
 </head>
 <body>
     <h1><a href="../index.php">Back to Home</a></h1>
